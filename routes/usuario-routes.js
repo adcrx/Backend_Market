93b88521a4cd9b440a91204e11f3db6da38dea18
adcrx <alejandra.cotroneo@gmail.com>
2025-04-08 21:27:58 -0400

    mayuscula a minuscula en nombre archivo usuario-controller

--- routes/usuario-routes.js
+++ routes/usuario-routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const usuarioController = require('../controllers/usuario-Controller');
+const usuarioController = require('../controllers/usuario-controller');
 
 // Ruta GET /usuarios para obtener todos los usuarios
 router.get('/', usuarioController.getUsuarios);
